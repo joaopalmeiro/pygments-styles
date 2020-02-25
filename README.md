9bbaccc77dd0a3d71907a314a35328e9435ea924
João Palmeiro <joaommpalmeiro@gmail.com>
2020-02-25 21:49:38 +0000

    Updated: CV style.

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 A set of custom Pygments styles.
 
-# Acknowledgments
+## Acknowledgments
 
 - [Pieter Belmans](https://github.com/pbelmans) ([_ua-pygments-style_](https://github.com/pbelmans/ua-pygments-style) repo).
--- styles/cv.py
+++ styles/cv.py
@@ -1,5 +1,5 @@
 from pygments.style import Style
-from pygments.token import Name, Comment, String, Number, Operator, Punctuation
+from pygments.token import Name, Comment, String, Number, Operator, Punctuation, Keyword
 
 class CVStyle(Style):
     default_style = ""
@@ -8,6 +8,13 @@ class CVStyle(Style):
     grey_color = "#CFCFCF"
     
     styles = {
-        Comment: 'italic ' + grey_color
+        Comment: 'italic ' + grey_color,
+
+        Punctuation: black_color,
+        Number: black_color,
+        Operator: black_color,
+        Name: black_color,
+        String: black_color,
+        Keyword: black_color   
     }
 
